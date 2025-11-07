b0e17da66528b4e4acc9f53af6d519f51dbb7ea3
logicscienc <anjuukumari001@gmail.com>
2025-11-07 22:28:41 +0530

    new commit

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,8 +1,6 @@
-const BASE_URL = process.env.REACT_APP_BASE_URL
-
-
+const BASE_URL = process.env.REACT_APP_BASE_URL; 
 
 export const PRODUCT_API = {
-  GET_ALL_PRODUCTS: "/",
-  GET_PRODUCT_BY_SLUG: (slug) => `/${slug}`,
+  GET_ALL_PRODUCTS: `${BASE_URL}/api/v1/product`,         
+  GET_PRODUCT_BY_SLUG: (slug) => `${BASE_URL}/api/v1/product/${slug}`, 
 };
